cdb6ef8dd8390066eefd9b7d7fa9b3e19877e81a
joseph <white.joseph.jr@gmail.com>
2023-12-12 23:07:07 -0600

    add readme

--- app/index.js
+++ app/index.js
@@ -5,6 +5,8 @@ import { Link } from "expo-router";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
+      <Text className="mb-5">This will be the Landing Screen</Text>
+
       <Link href="/home" asChild>
         <Text>Click here to go to the Home Screen</Text>
       </Link>
